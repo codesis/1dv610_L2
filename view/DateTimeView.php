e512355e08cbb30dc40fbc89f8949e8a9a44ac77
codesis <hassling.emma@gmail.com>
2019-09-20 11:32:17 +0200

    changed DateTimeView to be more clean and readable

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -4,8 +4,14 @@ class DateTimeView {
 
 
 	public function show() {
+
+		$dayOfWeek = date('l');
+		$dateOfDay = date('jS');
+		$month = date('F ');
+		$year = date('Y');
+		$time = date('H:i:s');
 		
-		$timeString = date('l\, \t\h\e jS \of F Y\, \T\h\e \t\i\m\e \i\s H:i:s');
+		$timeString = $dayOfWeek . ', the ' . $dateOfDay . ' of ' . $month . $year . ', The time is ' . $time;
 
 		return '<p>' . $timeString . '</p>';
 	}
