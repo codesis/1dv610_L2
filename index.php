c0236b1902eeccf012697712e595a81c0289cc5b
codesis <hassling.emma@gmail.com>
2019-09-24 12:47:30 +0200

    ini set for security

--- index.php
+++ index.php
@@ -16,7 +16,9 @@ $v = new LoginView();
 $dtv = new DateTimeView();
 $lv = new LayoutView();
 
+ini_set('session.cookie_secure', 1);
 ini_set('session.cookie_httponly', 1);
+ini_set('session.use_only_cookies', 1);
 session_start();
 
 $v->login();
