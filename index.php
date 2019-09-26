e54107971333a391cd6d5f7a7d7bc85f14c0196a
codesis <hassling.emma@gmail.com>
2019-09-26 15:23:08 +0200

    added faulty register messages

--- index.php
+++ index.php
@@ -16,15 +16,7 @@ $r = new RegisterView();
 $dtv = new DateTimeView();
 $lv = new LayoutView();
 
-session_start();
-if (isset($_SESSION['HTTP_USER_AGENT'])) {
-    if ($_SESSION['HTTP_USER_AGENT'] != md5($_SERVER['HTTP_USER_AGENT'])) {
-      session_unset();
-    }
-  } else {
-    $_SESSION['HTTP_USER_AGENT'] = md5($_SERVER['HTTP_USER_AGENT']);
-  }
-  
+session_start();  
 
 $v->login();
 if (isset($_SESSION['username'])) {
--- view/RegisterView.php
+++ view/RegisterView.php
@@ -8,6 +8,7 @@ class RegisterView {
 	private static $passwordRepeat = 'RegisterView::PasswordRepeat';
 	private static $messageId = 'RegisterView::Message';
 	private $message = '';
+	private $holdUsername = '';
 
 
 	/**
@@ -16,10 +17,28 @@ class RegisterView {
 	* @return  void, BUT writes to standard output!
 	*/
 	private function register () {
-		if (isset($_POST['register'])) {
-			$this->message = 'pressed register button';
+		if (isset($_POST[self::$register])) {
+			$this->faultyRegisterCredentials();
 		}
 	}
+	private function faultyRegisterCredentials () {
+		if (empty($_POST[self::$name]) && empty($_POST[self::$password]) && empty($_POST[self::$passwordRepeat])) {
+			$this->message = 'Username has too few characters, at least 3 characters. Password has too few characters, at least 6 characters.';
+		} else if (strlen($_POST[self::$name]) >= 3 && (empty($_POST[self::$password]) || strlen($_POST[self::$password]) <=5) && $_POST[self::$password] === $_POST[self::$passwordRepeat]) {
+			$this->holdUsername = $_POST[self::$name];
+			$this->message = 'Password has too few characters, at least 6 characters.';
+		} else if (strlen($_POST[self::$name]) <= 2 && strlen($_POST[self::$password] >= 6) && $_POST[self::$password] === $_POST[self::$passwordRepeat]) {
+			$this->holdUsername = $_POST[self::$name];
+			$this->message = 'Username has too few characters, at least 3 characters.';
+		} else if (strlen($_POST[self::$name]) >= 3 && strlen($_POST[self::$password]) >= 6 && $_POST[self::$password] != $_POST[self::$passwordRepeat]) {
+			$this->holdUsername = $_POST[self::$name];
+			$this->message = 'Passwords do not match.';
+		} else if ($_POST[self::$name] === 'Admin' && strlen($_POST[self::$password] >= 6) && $_POST[self::$password] === $_POST[self::$passwordRepeat]) {
+			$this->holdUsername = $_POST[self::$name];
+			$this->message = 'User exists, pick another username.';
+		} 
+	}
+
 	public function response () {
 		$this->register();
 		if (isset($_GET['register'])) {
@@ -34,7 +53,7 @@ class RegisterView {
 					<p id="' . self::$messageId . '">' . $message . '</p>
 					
 					<label for="' . self::$name . '">Username :</label>
-					<input type="text" id="' . self::$name . '" name="' . self::$name . '" value="" />
+					<input type="text" id="' . self::$name . '" name="' . self::$name . '" value="' . $this->holdUsername . '" />
 
 					<label for="' . self::$password . '">Password :</label>
 					<input type="password" id="' . self::$password . '" name="' . self::$password . '" value="" />
