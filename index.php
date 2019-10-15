40cf7e50fdd836c8f9bb58c720fdd6f17ac62e75
codesis <hassling.emma@gmail.com>
2019-10-15 13:59:41 +0200

    commenting out error reporting

--- index.php
+++ index.php
@@ -8,8 +8,8 @@ require_once('controller/AppController.php');
 require_once('model/Database.php');
 
 //MAKE SURE ERRORS ARE SHOWN... MIGHT WANT TO TURN THIS OFF ON A PUBLIC SERVER
-error_reporting(E_ALL);
-ini_set('display_errors', 'On');
+// error_reporting(E_ALL);
+// ini_set('display_errors', 'On');
 
 $database = new \model\Database($dbServer, $dbName, $dbUsername, $dbPassword);
 
